63584ec7c5397886f0bfce8855eeef39faa06bbc
German Antognetti <German.Antognetti@SEMANTOOLS.COM>
2018-01-16 08:50:21 -0300

    First sample and project configuration. Add a simple website, add first sample and libs to test with qunit.

---
+++ JsSamples/JsSamples/scripts/samples/sample1.js
@@ -0,0 +1,48 @@
+﻿'use strinct'
+
+var sample1 = (function () {
+    var getFlatten = function (nestedArray) {
+        var flatArray = new Array();
+
+        for (let index = 0; index < nestedArray.length; index++) {
+            console.log(nestedArray[index]);
+            if (Array.isArray(nestedArray[index])) {
+                flatArray = flatArray.concat(nestedArray[index]);
+            } else {
+                flatArray.push(nestedArray[index]);
+            }
+
+        }
+        return flatArray;
+    };
+
+
+    var getFlattenRecursive = function (nestedArray, flatArray) {
+        var currentArray = flatArray || {};
+        if (nestedArray && nestedArray.length > 0) {
+            nestedArray.forEach(function (value, index, array) {
+                if (Array.isArray(value)) {
+                    getFlattenRecursive(value, currentArray);
+                }
+                else {
+                    flatArray.push(value);
+                }
+            });
+        }
+        return currentArray;
+    };
+
+    var getArrayToTest = function () {
+        var arrayToTest = new Array();
+        arrayToTest.push(1);
+        arrayToTest.push(new Array(2, 3, 4));
+        arrayToTest.push(new Array(5, 6));
+        arrayToTest.push(7);
+        return arrayToTest;
+    };
+    return {
+        getFlatten: getFlatten,
+        getArrayToTest: getArrayToTest,
+        getFlattenRecursive: getFlattenRecursive
+    }
+})();
