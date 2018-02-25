9d9a1545fe85fbec930f26d936b6c23488cf02a7
Germán Antognetti <g.antognetti@gmail.com>
2018-02-25 17:07:04 -0300

    Add new project

---
+++ JsSamples/DataRepository/Model/Pet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataRepository.Model
+{
+    public class Pet
+    {
+        [Key]
+        public long   Id    { get; set; }
+        public string Name  { get; set; }
+        public string Raze  { get; set; }
+        public int    Years { get; set; }
+    }
+}
